cabc74aae00776f008395343f2101bfedb16a10d
Ante <ante.hellgren@se.experis.com>
2021-08-12 08:49:57 +0200

    Added vuex state, and set methods.

--- src/store/index.js
+++ src/store/index.js
@@ -4,5 +4,16 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    
+    state: {
+        questions: [],
+        error: ''
+    },
+    mutations: {
+        setQuestions: (state, payload) => {
+            state.questions = payload;
+        },
+        setError: (state, payload) => {
+            state.error = payload;
+        }
+    }
 })
